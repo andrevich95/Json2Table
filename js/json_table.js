279bfde02b578bf878dca9646e4467052396c915
andrew <andrey@innstant.com>
2018-09-23 00:05:26 +0300

    Converted to Jquery Plugin

--- js/json_table.js
+++ js/json_table.js
@@ -1,3 +1,47 @@
+$.fn.jsonTable = function(){
+    const table_block = this;
+    const table = $(this).find('table');
+    const tbody = $(table).find('tbody');
+    const textarea = $(this).find('#json-holder');
+    const error_msg = $(this).find('#error-msg');
+    table_block.on('click','#add-json', function (e) {
+        e.preventDefault();
+        $(error_msg).hide();
+        let val = $(textarea).val();
+        string2table(val, $(table));
+    }).on('click','#load-json', function(e){
+        e.preventDefault();
+        $(error_msg).hide();
+        let json = rows2json($(table));
+        $(textarea).val(JSON.stringify(json));
+    }).on('click','#replace-json', function(e){
+        e.preventDefault();
+        $(error_msg).hide();
+        $(tbody).html('');
+        $(error_msg).hide();
+        let val = $(textarea).val();
+        string2table(val, $(table));
+    }).on('change','#file-load',function (e) {
+        $(error_msg).hide();
+        const input = this;
+        const url = $(this).val();
+        const ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
+        if (input.files && input.files[0] && (ext === "csv" || ext === "json"))
+        {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                string2table(e.target.result,$(table));
+            };
+            reader.readAsText(input.files[0]);
+        }
+        else
+        {
+            $(error_msg).html('Error loading file').show();
+        }
+        input.replaceWith(input.val('').clone(true));
+    });
+};
+
 function string2table(string, table){
     $.parseJSON(string).forEach(function(el){
         $(table).find('tbody').append(obj2row(el));
@@ -29,8 +73,4 @@ function writefile(filename, table){
     file.open("w"); // open file with write access
     file.writeln(JSON.stringify(rows2json(table)));
     file.close();
-}
-
-function loadfile(){
-
 }
