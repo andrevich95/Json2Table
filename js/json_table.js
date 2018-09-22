beb94f2ec9cf0b68d72e53c3b2209f2288f4b458
andrew <andrey@innstant.com>
2018-09-22 23:49:35 +0300

    Functions fullfiled

--- js/json_table.js
+++ js/json_table.js
@@ -6,9 +6,13 @@ function string2table(string, table){
 
 function rows2json(table){
     let tbody = $(table).find('tbody');
-    tbody.find('tr').each(function(elem){
-        alert(elem);
+    var array = [];
+    tbody.find('tr').each(function(){
+        let name = $(this).find('td:nth-child(1)').text();
+        let value = $(this).find('td:nth-child(2)').text();
+        array.push({name:name, value:value});
     });
+    return array;
 }
 
 function obj2row(obj){
@@ -18,4 +22,15 @@ function obj2row(obj){
         '<button class="btn delete-action"><span class="fa fa-trash-o"></span></button>';
     tr.append(td.clone().text(obj.name)).append(td.clone().text(obj.value)).append(td.clone().html(actions));
     return tr;
+}
+
+function writefile(filename, table){
+    let file = new File([""], filename);
+    file.open("w"); // open file with write access
+    file.writeln(JSON.stringify(rows2json(table)));
+    file.close();
+}
+
+function loadfile(){
+
 }
