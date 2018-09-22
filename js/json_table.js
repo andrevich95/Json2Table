5e1a7200081b596706df6fd442db0bd20924b396
andrew <andrey@innstant.com>
2018-09-23 00:31:41 +0300

    Added exception Handling and editing rows

--- js/json_table.js
+++ js/json_table.js
@@ -1,44 +1,83 @@
 $.fn.jsonTable = function(){
+    //Initialize variables
     const table_block = this;
     const table = $(this).find('table');
     const tbody = $(table).find('tbody');
     const textarea = $(this).find('#json-holder');
     const error_msg = $(this).find('#error-msg');
+
+    //Actions on main area
     table_block.on('click','#add-json', function (e) {
         e.preventDefault();
         $(error_msg).hide();
         let val = $(textarea).val();
-        string2table(val, $(table));
+        try {
+            string2table(val, $(table));
+        }catch (e) {
+            $(error_msg).html(e.message).show();
+        }
     }).on('click','#load-json', function(e){
         e.preventDefault();
         $(error_msg).hide();
-        let json = rows2json($(table));
-        $(textarea).val(JSON.stringify(json));
+        try{
+            let json = rows2json($(table));
+            $(textarea).val(JSON.stringify(json));
+        }catch (e) {
+            $(error_msg).html(e.message).show();
+        }
     }).on('click','#replace-json', function(e){
         e.preventDefault();
         $(error_msg).hide();
         $(tbody).html('');
         $(error_msg).hide();
         let val = $(textarea).val();
-        string2table(val, $(table));
+        try{
+            string2table(val, $(table));
+        }catch (e) {
+            $(error_msg).html(e.message).show();
+        }
     }).on('change','#file-load',function (e) {
         $(error_msg).hide();
         const input = this;
         const url = $(this).val();
         const ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
-        if (input.files && input.files[0] && (ext === "csv" || ext === "json"))
-        {
+        if (input.files && input.files[0] && (ext === "csv" || ext === "json")) {
             const reader = new FileReader();
             reader.onload = function (e) {
                 string2table(e.target.result,$(table));
             };
             reader.readAsText(input.files[0]);
         }
-        else
-        {
+        else {
             $(error_msg).html('Error loading file').show();
         }
         input.replaceWith(input.val('').clone(true));
+    }).on('click', '.delete-action', function(){
+        $(this).parents('tr').remove();
+    }).on('click', '.edit-action', function(){
+        $(this).find('span').removeClass('fa-pencil');
+        $(this).find('span').addClass('fa-save');
+
+        $(this).removeClass('edit-action');
+        $(this).addClass('save-action');
+
+        let key = $(this).parents('tr').find('td:nth-child(1)');
+        let value = $(this).parents('tr').find('td:nth-child(2)');
+
+        $(key).html('<input type="text" value="'+$(key).text()+'"/>');
+        $(value).html('<input type="text" value="'+$(value).text()+'"/>');
+    }).on('click', '.save-action', function(){
+        $(this).find('span').removeClass('fa-save');
+        $(this).find('span').addClass('fa-pencil');
+
+        $(this).removeClass('save-action');
+        $(this).addClass('edit-action');
+
+        let key = $(this).parents('tr').find('td:nth-child(1)');
+        let value = $(this).parents('tr').find('td:nth-child(2)');
+
+        $(key).text($(key).find('input').val());
+        $(value).text($(value).find('input').val());
     });
 };
 
